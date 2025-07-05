212b0a08675a8db39649687e5b5a0c5390a18c00
Munir Mujuzi <munirumujuzi@gmail.com>
2025-07-05 12:29:35 +0300

    This the first test we want to be executed

---
+++ multiplication_tdd/test/test.js
@@ -0,0 +1,8 @@
+const assert = require('chai').assert;
+const multiply = require('../multiply');
+
+describe('Multiply', () => {
+  it('1x1=1', () => {
+    assert.strictEqual(multiply(1, 1), 1);
+  });
+});
