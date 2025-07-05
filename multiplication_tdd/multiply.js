1bdd6b52aef4f3bf7fb08f08fd375e8433e7b164
Mutesi <joanahmutesi38@gmail.com>
2025-07-05 18:57:04 +0300

    The 3rd test has passed with the implementation of a conditional statement

--- multiplication_tdd/multiply.js
+++ multiplication_tdd/multiply.js
@@ -1,5 +1,8 @@
 function multiply(a, b) {
-  if (a === 2 && b === 2) return 4;
+  if (a === 2 && b === 2) 
+    return 4;
+  if (a === 3 && b === 3) 
+    return 9;
   return 1;
 }
 module.exports = multiply;
