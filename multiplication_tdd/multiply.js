33b3dbaa170011e31ca1df3f2ebb634ba2f3dd1c
Mutesi <joanahmutesi38@gmail.com>
2025-07-05 19:20:12 +0300

    The 4th test has passed with the implementation of a condition statement

--- multiplication_tdd/multiply.js
+++ multiplication_tdd/multiply.js
@@ -3,6 +3,8 @@ function multiply(a, b) {
     return 4;
   if (a === 3 && b === 3) 
     return 9;
+  if (a === 4 && b === 4)
+    return 16;
   return 1;
 }
 module.exports = multiply;
--- multiplication_tdd/test/test.js
+++ multiplication_tdd/test/test.js
@@ -6,9 +6,12 @@ describe('Multiply', () => {
     assert.strictEqual(multiply(1, 1), 1);
   });
   it('2x2=4', () => {
-  assert.strictEqual(multiply(2, 2), 4);
+    assert.strictEqual(multiply(2, 2), 4);
   });
   it('3x3=9', () => {
-  assert.strictEqual(multiply(3, 3), 9);
-});
+    assert.strictEqual(multiply(3, 3), 9);
+  });
+  it('4x4=16', () => {
+    assert.strictEqual(multiply(4, 4), 16);
+  });
 });
