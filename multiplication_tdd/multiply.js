11487770de8c26f5c87c75e636b041c980087712
Munir Mujuzi <munirumujuzi@gmail.com>
2025-07-05 20:41:18 +0300

    we refactored the code using a for loop to pass all the test

--- multiplication_tdd/multiply.js
+++ multiplication_tdd/multiply.js
@@ -1,10 +1,26 @@
-function multiply(a, b) {
-  if (a === 2 && b === 2) 
-    return 4;
-  if (a === 3 && b === 3) 
-    return 9;
-  if (a === 4 && b === 4)
-    return 16;
-  return 1;
+// function multiply(a, b) {
+//   if (a === 2 && b === 2) 
+//     return 4;
+//   if (a === 3 && b === 3) 
+//     return 9;
+//   if (a === 4 && b === 4)
+//     return 16;
+//   return 1;
+// }
+// module.exports = multiply;
+
+
+function multiply(a, b) {  
+  if (a === 0 || b === 0) return 0; 
+  const X = (a < 0) !== (b < 0);
+  const A = Math.abs(a);
+  const B = Math.abs(b);
+  
+  let result = 0;  
+  for (let i = 0; i < B; i++) {
+    result += A;
+  }  
+  return X ? -result : result;
 }
+
 module.exports = multiply;
