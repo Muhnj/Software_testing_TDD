acd6d7e9763a9306343774448e97a7fcf85018d0
Munir Mujuzi <munirumujuzi@gmail.com>
2025-07-08 08:22:42 +0300

    updated method of testing

--- factorial/test/test.js
+++ factorial/test/test.js
@@ -1,29 +1,28 @@
-const { expect } = require('chai');
+const assert = require('assert');
 const factorial = require('../factorial');
 
 describe('Factorial Function', () => {
-    describe('Factorial Function', () => {
   it('0! should be 1', () => {
-    expect(factorial(0)).to.equal(1);
+    assert.strictEqual(factorial(0), 1);
   });
-  
+
   it('1! should be 1', () => {
-    expect(factorial(1)).to.equal(1);
+    assert.strictEqual(factorial(1), 1);
   });
-  
+
   it('5! should be 120', () => {
-    expect(factorial(5)).to.equal(120);
+    assert.strictEqual(factorial(5), 120);
   });
+
   it('Negative input should return null', () => {
-    expect(factorial(-1)).to.be.null;
+    assert.strictEqual(factorial(-1), null);
   });
-  
+
   it('Non-integer input should return null', () => {
-    expect(factorial(2.5)).to.be.null;
+    assert.strictEqual(factorial(2.5), null);
   });
-  
+
   it('10! should be 3,628,800', () => {
-    expect(factorial(10)).to.equal(3628800);
+    assert.strictEqual(factorial(10), 3628800);
   });
-});
 });
