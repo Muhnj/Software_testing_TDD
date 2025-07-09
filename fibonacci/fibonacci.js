8823eafa62019428f9e6f011c4828d9b94a43793
Munir Mujuzi <munirumujuzi@gmail.com>
2025-07-09 10:39:49 +0300

    we refactored the solution to Iterate all the fibonacci numbers

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -5,9 +5,17 @@
 function fibonacci(n) {
   if (n === 0) return 0;
   if (n === 1) return 1;
-  if (n === 2) return 1;
-  if (n === 3) return 2;
-  return 3; 
+  
+  let a = 0;
+  let b = 1;
+  
+  for (let i = 2; i <= n; i++) {
+    const next = a + b;
+    a = b;
+    b = next;
+  }
+  
+  return b;
 }
 
 module.exports = fibonacci;
