5bf2b9e34d53c0a1450ab0601138604744ec2887
Munir Mujuzi <munirumujuzi@gmail.com>
2025-07-09 10:44:02 +0300

    all the tests passed and the refactored code works very well

--- fibonacci/test/test.js
+++ fibonacci/test/test.js
@@ -9,14 +9,21 @@ describe('Fibonacci', () => {
     assert.strictEqual(fibonacci(1), 1);
   });
   it('should return 1 for input 2', () => {
-  assert.strictEqual(fibonacci(2), 1);
+    assert.strictEqual(fibonacci(2), 1);
   });
   it('should return 2 for input 3', () => {
-  assert.strictEqual(fibonacci(3), 2);
+    assert.strictEqual(fibonacci(3), 2);
   });
   it('should return 3 for input 4', () => {
-  assert.strictEqual(fibonacci(4), 3);
+    assert.strictEqual(fibonacci(4), 3);
   });
-
-
+  it('should return 5 for input 5', () => {
+    assert.strictEqual(fibonacci(5), 5);
+  });
+  it('should return 8 for input 6', () => {
+    assert.strictEqual(fibonacci(6), 8);
+  });
+  it('should return 55 for input 10', () => {
+    assert.strictEqual(fibonacci(10), 55);
+  });
 });
