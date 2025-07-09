b21aa386e478e93dcbaadbdfc7aef1ab9aab00d3
Munir Mujuzi <munirumujuzi@gmail.com>
2025-07-09 10:06:00 +0300

    adding the second test

--- fibonacci/test/test.js
+++ fibonacci/test/test.js
@@ -5,4 +5,7 @@ describe('Fibonacci', () => {
   it('should return 0 for input 0', () => {
     assert.strictEqual(fibonacci(0), 0);
   });
+  it('should return 1 for input 1', () =>{
+    assert.strictEqual(fibonacci(1), 1);
+  });
 });
