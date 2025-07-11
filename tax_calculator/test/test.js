7d2232434135ce3e531d95a6a5f9b9971da7e547
Munir Mujuzi <munirumujuzi@gmail.com>
2025-07-11 10:29:20 +0300

    implementing the tax 40% failing test

--- tax_calculator/test/test.js
+++ tax_calculator/test/test.js
@@ -2,9 +2,9 @@ const assert = require('assert');
 const calculateTax = require('../taxCalculator');
 
 describe('Tax Calculator', () => {
- it('should calculate 20% tax on earnings between 12,000 and 36,000', () => { 
-  assert.strictEqual(calculateTax(12000), 0); 
-  assert.strictEqual(calculateTax(20000), 1600);  
-  assert.strictEqual(calculateTax(36000), 4800);
- });
+ it('should calculate 40% tax on earnings above 36,000', () => {  
+  assert.strictEqual(calculateTax(36000), 4800);  
+  assert.strictEqual(calculateTax(40000), 6400);  
+  assert.strictEqual(calculateTax(50000), 10400);
+});
 });  
