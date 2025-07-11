a7b17016454e2faa7374ce0b6fb90e32dea6fc9f
Munir Mujuzi <munirumujuzi@gmail.com>
2025-07-11 10:31:41 +0300

    implementation of the passing test for the 40% tax

--- tax_calculator/taxCalculator.js
+++ tax_calculator/taxCalculator.js
@@ -1,6 +1,10 @@
 function calculateTax(earnings) {
-  if (earnings <= 12000) return 0;
-  return (earnings - 12000) * 0.2;
+  if (earnings <= 12000) return 0;  
+  if (earnings <= 36000) {
+    return (earnings - 12000) * 0.2;
+  }
+   
+  return (earnings - 36000) * 0.4 + 4800;
 }
 
 module.exports = calculateTax;
